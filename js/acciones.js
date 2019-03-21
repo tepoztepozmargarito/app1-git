dcd8c52ad21d349a14ce878e98e92b04a49a1210
yahir <yahirruizmen0@gmail.com>
2019-03-21 08:55:07 -0600

    1 subida

--- js/acciones.js
+++ js/acciones.js
@@ -1,3 +1,12 @@
 // JavaScript Document
 
+$(document).ready(function(e) {
+    document.addEventListener("deviceready",function(){
+		$('#disp table td').eq(3).text(device.model);
+		$('#disp table td').eq(3).text(device.cordova);
+		$('#disp table td').eq(3).text(device.platform);
+		$('#disp table td').eq(3).text(device.version);
+		$('#disp table td').eq(3).text(device.uuid);
+	},false);//ready device
 
+});//document
